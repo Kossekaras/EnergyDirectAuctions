b4db00da7445a746b2fabee72c63a8ba3a5d8449
Konstantinos Sekaras <ksekas7@gmail.com>
2026-06-22 06:34:15 +0300

    Update comment

--- project_files/contracts/EnergyAuction.sol
+++ project_files/contracts/EnergyAuction.sol
@@ -37,7 +37,7 @@ contract EnergyAuction is usingProvable {
 		deployer = msg.sender;
 	}
 
-	// final addition - previously deployer first called 'newCycle'
+	//final addition - previously deployer first called 'newCycle'
 	function initiate () public {
 		require(msg.sender == deployer, "Only the deployer can make this call");
 		if (period == 0) {
